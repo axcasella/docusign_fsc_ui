4c24058347d98505e5b79735b83ff96703e14f42
vidhu <vidhu1911@gmail.com>
2020-08-26 23:50:06 -0400

    hostname in docusign redirect

--- src/services/docusign/constants.ts
+++ src/services/docusign/constants.ts
@@ -1,4 +1,4 @@
-const REDIRECT_URI = 'http://localhost:3000/signingComplete';
+const REDIRECT_URI = `${window.location.origin}/signingComplete`;
 
 export const DOCUSIGN_OAUTH_URL = `https://account-d.docusign.com/oauth/auth?response_type=code&scope=signature&client_id=08ecb933-13e3-45e7-aed3-090418488db8&redirect_uri=${REDIRECT_URI}`;
 export const DOCUSIGN_EXCHANGE_TOKEN_URL =
