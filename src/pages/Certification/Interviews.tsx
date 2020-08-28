5e2f7d448d056c8f49f48dca826a0cc0f9057238
Alex Casella <alex.casella@ibm.com>
2020-08-27 21:30:56 -0400

    updated employee names

--- src/pages/Certification/Interviews.tsx
+++ src/pages/Certification/Interviews.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Typography, Row, Col, Checkbox } from 'antd';
-import TimelineControl from './TimelineControl';
-import { CertificationStep } from 'store/certification/types';
-import { ZoomLink } from 'components';
+import React from "react";
+import { Typography, Row, Col, Checkbox } from "antd";
+import TimelineControl from "./TimelineControl";
+import { CertificationStep } from "store/certification/types";
+import { ZoomLink } from "components";
 
 const PEOPLE_INFO = [
-  { label: 'Liam Welsh', value: 'Liam.Welsh@gmail.com' },
-  { label: 'Emma Titer', value: 'Emma.Titer@gmail.com' },
-  { label: 'Noah Hanes', value: 'Noah.Hanes@gmail.com' },
-  { label: 'Olivia Richmond', value: 'Olivia.Richmond@gmail.com' },
-  { label: 'William Trois', value: 'William.Trois@gmail.com' },
-  { label: 'Ava Potter', value: 'Ava.Potter@gmail.com' },
+  { label: "Jim Halpert", value: "Liam.Welsh@gmail.com" },
+  { label: "Pam Beesly", value: "Emma.Titer@gmail.com" },
+  { label: "Dwight Schrute", value: "Noah.Hanes@gmail.com" },
+  { label: "Kelly Kapoor", value: "Olivia.Richmond@gmail.com" },
+  { label: "Ryan Howard", value: "William.Trois@gmail.com" },
+  { label: "Kevin Malone", value: "Ava.Potter@gmail.com" },
 ];
 
 const Interviews = () => (
@@ -26,11 +26,12 @@ const Interviews = () => (
       </Col>
       <Col md={{ span: 16 }}>
         <Typography.Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu fringilla nulla. Cras euismod erat at
-          felis semper, at tincidunt nibh facilisis. Suspendisse potenti.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          eu fringilla nulla. Cras euismod erat at felis semper, at tincidunt
+          nibh facilisis. Suspendisse potenti.
         </Typography.Paragraph>
         <Typography.Paragraph>
-          <ZoomLink link="https://zoom.us/j/92401797536" />
+          <ZoomLink link='https://zoom.us/j/92401797536' />
         </Typography.Paragraph>
       </Col>
     </Row>
